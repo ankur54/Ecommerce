4bc0941e9ec818a14178b873a6a1d9259bdb25ec
sachin tom <sachintomacc@gmail.com>
2020-07-26 15:51:50 +0530

    updated

--- core/templatetags/cart_template_tags.py
+++ core/templatetags/cart_template_tags.py
@@ -1,6 +1,6 @@
 from django import template
-from core.models import OrderItem
-from django.db.models import Sum
+from core.models import OrderItem,WishList
+from django.db.models import Sum,Count
 
 
 register = template.Library()
@@ -8,8 +8,17 @@ register = template.Library()
 @register.filter
 def cart_item_count(user):
     if user.is_authenticated:
-        # return OrderItem.objects.filter(user=user,ordered=False).count()
-        # return OrderItem.objects.aggregate(Sum('quantity'))['quantity__sum'] or 0
         return OrderItem.objects.filter(user=user,ordered=False).aggregate(Sum('quantity'))['quantity__sum'] or 0
+    else:
+        return 0
+
+
+@register.filter
+def wishlist_item_count(user):
+    if user.is_authenticated:
+        # Keyword.objects.all().annotate(article_count=models.Count('article'))[0].article_count
+        # return WishList.objects.filter(user=user).annotate(item_count=Count('items')) or 0
+        return WishList.objects.filter(user=user).annotate(item_count=Count('items'))
+        WishList.objects.filter(user=user).annotate(cnt=Count('items')).order_by('-cnt')[0]
     else:
         return 0
