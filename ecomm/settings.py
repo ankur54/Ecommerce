1e0d0bc87761da0b448bb5fb7de732c4b0f94494
gilbish <gilbeeshkosma15@gmail.com>
2020-07-30 15:28:16 +0530

    sqlite setup locally postgres on production

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -1,13 +1,19 @@
 
 import os
 from django.contrib.messages import constants as messages
-
+import dj_database_url
+import dotenv
+import django_heroku
 
 from django.conf.global_settings import STATICFILES_DIRS
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -86,12 +92,9 @@ WSGI_APPLICATION = 'ecomm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
 
 
 # Password validation
@@ -156,3 +159,11 @@ STRIPE_KEY = "sk_12345"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ACCOUNT_LOGOUT_ON_GET = True
 ADMIN_ENABLED = False
+
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
+#For Heroku
+del DATABASES['default']['OPTIONS']['sslmode']
